c14cfe16443e9318e64161c36dc93dd94ddbab1e
Rohit Yadav <bhaisaab@baagi.org>
2014-08-16 12:38:05 +0200

    update dotfiles
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,6 +1,5 @@
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
-
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
@@ -29,7 +28,7 @@ DISABLE_AUTO_UPDATE="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git python ant osx brew go golang tmux)
+plugins=(git) # python ant osx brew go golang tmux)
 
 source $ZSH/oh-my-zsh.sh
 
